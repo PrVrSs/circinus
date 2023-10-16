2181bc9c36998951f6d93421b45bf7e4b88defcc
PrVrSs <resh.sersh@gmail.com>
2023-10-16 23:50:54 +0600

    add example

---
+++ notebook/blob.js
@@ -0,0 +1,45 @@
+// example 1
+
+const obj = { hello: "world" };
+const blob = new Blob([JSON.stringify(obj, null, 2)], {
+  type: "application/json",
+});
+
+// example 2
+
+function showViewLiveResultButton() {
+  if (window.self !== window.top) {
+    // Ensure that if our document is in a frame, we get the user
+    // to first open it in its own tab or window. Otherwise, this
+    // example won't work.
+    const p = document.querySelector("p");
+    p.textContent = "";
+    const button = document.createElement("button");
+    button.textContent = "View live result of the example code above";
+    p.append(button);
+    button.addEventListener("click", () => window.open(location.href));
+    return true;
+  }
+  return false;
+}
+
+if (!showViewLiveResultButton()) {
+  function typedArrayToURL(typedArray, mimeType) {
+    return URL.createObjectURL(
+      new Blob([typedArray.buffer], { type: mimeType }),
+    );
+  }
+  const bytes = new Uint8Array(59);
+
+  for (let i = 0; i < 59; i++) {
+    bytes[i] = 32 + i;
+  }
+
+  const url = typedArrayToURL(bytes, "text/plain");
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.innerText = "Open the array URL";
+
+  document.body.appendChild(link);
+}
