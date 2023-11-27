2e391d194e5334baa41777d3268c87b428116747
PrVrSs <resh.sersh@gmail.com>
2023-11-27 22:04:07 +0600

    update `agent`

--- circinus/agent.py
+++ circinus/agent.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 from attrs import define
@@ -5,6 +6,10 @@ from attrs import define
 from circinus.fuzzer import UserInput, candidate_prompt, fuzzing_loop
 from circinus.knowledge_base import ChunkDTO, init_knowledge_base
 from circinus.llm import GPT
+from circinus.vector_store import ContextFilter
+
+
+GENERATION_PROMPT = 'Please create a program which uses random class according to the following description'
 
 
 @define(hash=True)
@@ -12,6 +17,7 @@ class Source:
     file: str
     page: str
     text: str
+    type_: str
 
     @staticmethod
     def curate_sources(sources: list[ChunkDTO]) -> set['Source']:
@@ -22,8 +28,9 @@ class Source:
 
             file_name = doc_metadata.get('file_name', '-') if doc_metadata else '-'
             page_label = doc_metadata.get('page_label', '-') if doc_metadata else '-'
+            type_ = doc_metadata.get('type_ ', '-') if doc_metadata else '-'
 
-            source = Source(file=file_name, page=page_label, text=chunk.text)
+            source = Source(file=file_name, page=page_label, text=chunk.text, type_=type_)
             curated_sources.add(source)
 
         return curated_sources
@@ -33,6 +40,7 @@ class Source:
             f'{self.file=}',
             f'{self.page=}',
             f'{self.text=}',
+            f'{self.type_=}',
         ])
 
 
@@ -44,17 +52,36 @@ class Agent:
     def search_in_docs(self):
         pass
 
-    def add_docs(self, file_name: str, file_data: str | bytes) -> None:
-        self.knowledge_base.add(file_name, file_data)
+    def add_docs(self, file_name: str, file_data: str | bytes, type_) -> None:
+        self.knowledge_base.add(file_name, file_data, type_)
 
-    def query_docs(self, message: str):
+    def query_docs(self, message: str, context_filter: ContextFilter | None = None):
         return Source.curate_sources(
             sources=self.knowledge_base.retrieve(
                 text=message,
+                context_filter=context_filter,
                 limit=4,
                 prev_next_chunks=0,
             ),
         )
+
+    def fuzzing(self, target: str, generation_prompt: str = GENERATION_PROMPT):
+        documentation = self.query_docs(target, ContextFilter(doc_type='documentation', docs_ids=None))
+        specification = self.query_docs(target, ContextFilter(doc_type='specification', docs_ids=None))
+        code = self.query_docs(target, ContextFilter(doc_type='code', docs_ids=None))
+
+        prompts = candidate_prompt(
+            llm=self._llm,
+            user_input=UserInput(
+                documentation=random.choice(list(documentation)).text,
+                specification=random.choice(list(specification)).text,
+                code=random.choice(list(code)).text,
+            ),
+            num_samples=2,
+        )
+
+        for prompt in prompts:
+            yield from fuzzing_loop(llm=self._llm, prompt=prompt, generation_prompt=generation_prompt)
 
     def fuzz(self, documentation: str, specification: str, code: str):
         prompts = candidate_prompt(
--- circinus/vector_store.py
+++ circinus/vector_store.py
@@ -14,24 +14,12 @@ BASE = Path(__file__).parent
 
 @define
 class ContextFilter:
+    doc_type: str
     docs_ids: list[str] | None
 
 
 def _chromadb_doc_id_metadata_filter(context_filter: ContextFilter | None) -> dict | None:
-    if context_filter is None or context_filter.docs_ids is None:
-        return {}
-    if len(context_filter.docs_ids) < 1:
-        return {'doc_id': '-'}
-
-    doc_filter_items = []
-    if len(context_filter.docs_ids) > 1:
-        doc_filter = {'$or': doc_filter_items}
-        for doc_id in context_filter.docs_ids:
-            doc_filter_items.append({'doc_id': doc_id})
-    else:
-        doc_filter = {'doc_id': context_filter.docs_ids[0]}
-
-    return doc_filter
+    return {'doc_type': context_filter.doc_type}
 
 
 class VectorStore:
